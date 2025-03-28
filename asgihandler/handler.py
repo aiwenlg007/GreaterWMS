f23f57a6ca5e0377d2d57963c168136e31193b1b
aiwenlg007 <aiwenlg007@163.com>
2025-03-28 17:13:18 +0800

    Update handler.py
    修改特定域名为用户自定义域名

--- asgihandler/handler.py
+++ asgihandler/handler.py
@@ -2,7 +2,7 @@ import aiohttp
 
 
 async def fetch_data(data):
-    url = 'https://po.56yhz.com/asgihandler/'
+    url = 'https://test.com/asgihandler/' # 用户这里自己修改
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=data) as response:
             response_text = await response.json()
